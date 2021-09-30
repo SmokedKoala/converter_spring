4ebb3ac2cd712d8fd4f00cd9620e916cadfd9fe6
SmokedKoala <k.azarov@inbox.ru>
2021-09-30 21:40:38 +0300

    fix

--- src/main/java/ru/testing/ValuesConverter.java
+++ src/main/java/ru/testing/ValuesConverter.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * service for converting and saving current data
+ */
 @Component
 public class ValuesConverter {
 
@@ -15,35 +18,70 @@ public class ValuesConverter {
     String valueAmountValue1;
     String value2;
 
+    /**
+     * constructor
+     * @param model data model for getting values
+     */
     @Autowired
     public ValuesConverter(ValuesModel model) {
         this.model = model;
     }
 
+    /**
+     * getter for first value
+     * @return name of first value
+     */
     public String getValue1() {
         return value1;
     }
 
+    /**
+     * setter for first value
+     * @param value1 name of first value
+     */
     public void setValue1(String value1) {
         this.value1 = value1;
     }
 
+    /**
+     * getter for amount of first value
+     * @return amount value
+     */
     public String getValueAmountValue1() {
         return valueAmountValue1;
     }
 
+    /**
+     * setter for amount of first value
+     * @param valueAmountValue1 amount value
+     */
     public void setValueAmountValue1(String valueAmountValue1) {
         this.valueAmountValue1 = valueAmountValue1;
     }
 
+    /**
+     * getter for second value
+     * @return name of second value
+     */
     public String getValue2() {
         return value2;
     }
 
+    /**
+     * setter for second value
+     * @param value2 name of second value
+     */
     public void setValue2(String value2) {
         this.value2 = value2;
     }
 
+    /**
+     * method for converting values and returns result
+     * @return if all data correct, result number,
+     * if amount is lower than 0 - 'Incorrect amount',
+     * if amount is not a number - 'Amount should be number',
+     * if different types values - 'Different type values'
+     */
     public String convert(){
         try {
             if (Double.valueOf(valueAmountValue1) <0)
@@ -60,6 +98,10 @@ public class ValuesConverter {
 
     }
 
+    /**
+     * method for getting list of value names
+     * @return list of string value names
+     */
     public List<String> getListOfValues(){
         List<Values> enums = model.getValues();
         List<String> valuesNames = enums.stream().map(Values::getName).collect(Collectors.toList());
--- src/main/java/ru/testing/ValuesModel.java
+++ src/main/java/ru/testing/ValuesModel.java
@@ -2,14 +2,21 @@ package ru.testing;
 
 
 import org.springframework.stereotype.Component;
-import ru.testing.Values;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * model for getting data
+ */
 @Component
 public class ValuesModel {
 
+    /**
+     * method for getting value scale
+     * @param name string name of value
+     * @return scale number of value
+     */
     public double getValue(String name){
         Values necessaryValue = null;
         for(Values value : Values.values())
@@ -18,6 +25,11 @@ public class ValuesModel {
         return necessaryValue.getScale();
     }
 
+    /**
+     * method for getting value type
+     * @param name string name of value
+     * @return type of value
+     */
     public String getNotation(String name){
         Values necessaryValue = null;
         for(Values value : Values.values())
@@ -26,6 +38,10 @@ public class ValuesModel {
         return necessaryValue.getNotation();
     }
 
+    /**
+     * method for getting list of enum values
+     * @return list of enum elements
+     */
     public List<Values> getValues(){
         List<Values> notationValues = new ArrayList<>();
         for(Values value : Values.values())
